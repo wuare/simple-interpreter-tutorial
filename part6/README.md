02a69946fd6ac76f9fc6aca7343e4919965ac1a7
wuare <wuareb@gmail.com>
2021-07-08 10:48:37 +0800

    Support expressions inside parentheses

---
+++ part6/README.md
@@ -0,0 +1,10 @@
+part6  
+
+#### goal
+Add parenthesized expressions to our grammar and implementing an interpreter that will be able to evaluate parenthesized expressions with arbitrarily deep nesting, like the expression `7 + 3 * (10 / (12 / (3 + 1) - 1))`.  
+
+
+#### grammar
+![grammar](https://github.com/wuare/simple-interpreter-tutorial/blob/master/part6/images/lsbasi_part6_grammar.png)  
+(the image from [Ruslan' Blog](https://ruslanspivak.com/lsbasi-part6/))  
+
---
+++ part6/src/main/java/top/wuare/part6/Token.java
@@ -0,0 +1,27 @@
+package top.wuare.part6;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * token
+ *
+ * @author wuare
+ * @date 2021/7/7
+ */
+@Data
+@AllArgsConstructor
+public class Token {
+
+    public static final int EOF = -1;
+    public static final int INTEGER = 1;
+    public static final int PLUS = 2;
+    public static final int MINUS = 3;
+    public static final int MUL = 4;
+    public static final int DIV = 5;
+    public static final int LPAREN = 6;
+    public static final int RPAREN = 7;
+
+    private int type;
+    private String value;
+}
